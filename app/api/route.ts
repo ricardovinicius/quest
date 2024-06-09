853c444deb4d7ec198f3df6694b1aea8c050923b
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-06-09 12:37:57 -0300

    fix: removing temp route

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic"; // defaults to auto
-
-export async function POST(request: Request) {
-  const res = await request.json();
-
-  console.log(res.username);
-
-  return Response.json(res);
-}
